4cd67fc497f9cb054df4fa916781d45f9b9db2f9
Gyuliz Hadzhieva <fg.hadzhieva@gmail.com>
2020-03-05 17:15:50 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		//target directory to put built js file
+		path: path.join(__dirname, "/dist"),
+		// filename
+		filename: "index_bundle.js"
+		//				publicPath: "/builds",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/index.html")
+		})
+	]
+};
